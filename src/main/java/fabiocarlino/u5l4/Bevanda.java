7f25069583b6c3e33c80895e42dd7fcf53fcc71d
Leon35-fc <leon.f35@alice.it>
2026-02-05 16:33:14 +0100

    Database generated.

---
+++ src/main/java/fabiocarlino/u5l4/Bevanda.java
@@ -0,0 +1,22 @@
+package fabiocarlino.u5l4;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "bevande")
+public class Bevanda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private int calorie;
+
+    @Column(nullable = false)
+    private double prezzo;
+}
---
+++ src/main/java/fabiocarlino/u5l4/U5l4Application.java
@@ -0,0 +1,13 @@
+package fabiocarlino.u5l4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5l4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5l4Application.class, args);
+	}
+
+}
